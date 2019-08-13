284d87a4622a6636651722e1244c2131a3591d70
Christian Hovenbitzer <christianhovenbitzer@christians-mbp.speedport_w_724v_typ_a_05011603_06_001>
2019-08-13 21:26:01 +0200

    refactor

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"path"
+	"strings"
 
 	"github.com/AnotherCoolDude/golang-angular/handlers"
 	"github.com/auth0-community/go-auth0"
@@ -15,6 +17,7 @@ import (
 var (
 	audience string
 	domain   string
+	token    string
 )
 
 func main() {
@@ -44,15 +47,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	todos, err := getTodos(token)
+	if err != nil {
+		log.Println("error getting todos")
+		log.Println(err)
+	}
+	fmt.Println(todos)
 }
 
 func setAuth0Variables() {
-	audience = os.Getenv("AUTH0_API_IDENTIFIER")
-	domain = os.Getenv("AUTH0_DOMAIN")
+	audience = "https://my-golang-api"
+	domain = "dev-3tt1ae45.auth0.com"
+	t, err := receiveToken()
+	if err != nil {
+		fmt.Println(err)
+	}
+	token = t
 }
 
 func authRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Request.Header.Set("Content-Type", "application/json")
 		var auth0Domain = "https://" + domain + "/"
 		client := auth0.NewJWKClient(auth0.JWKClientOptions{URI: auth0Domain + ".well-known/jwks.json"}, nil)
 		config := auth0.NewConfiguration(client, []string{audience}, auth0Domain, jose.RS256)
@@ -73,4 +89,65 @@ func authRequired() gin.HandlerFunc {
 func terminateWithError(statusCode int, message string, c *gin.Context) {
 	c.JSON(statusCode, gin.H{"error": message})
 	c.Abort()
+}
+
+func receiveToken() (string, error) {
+	url := "https://dev-3tt1ae45.eu.auth0.com/oauth/token"
+
+	payload := strings.NewReader("{\"client_id\":\"Vi72HiE8p3clPpdji4L64i0axSba3q1u\",\"client_secret\":\"ONmN5BSndMCyPCHqnswN9d5ycoenQ72-XHmmgqr4WfqzqVAPbwCpaAU-ilTKNWq5\",\"audience\":\"https://my-golang-api\",\"grant_type\":\"client_credentials\"}")
+
+	req, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		log.Println("error creating request")
+		return "", err
+	}
+
+	req.Header.Add("content-type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("error sending request")
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println("error reading response")
+		return "", err
+	}
+
+	fmt.Println(res)
+	fmt.Println()
+	fmt.Println(string(body))
+	return string(body), nil
+}
+
+func getTodos(bearerToken string) (string, error) {
+	url := "localhost:3000/todo"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("error creating request")
+		return "", err
+	}
+
+	req.Header.Add("authorization", "Bearer "+bearerToken)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("error sending request")
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println("error reading response")
+		return "", err
+	}
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+	return string(body), nil
 }
